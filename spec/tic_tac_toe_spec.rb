44aed9217221c0aab361577a0af194cbb6af67cb
hykare <78385964+hykare@users.noreply.github.com>
2022-08-07 14:02:05 +0200

    test Game#run

--- spec/tic_tac_toe_spec.rb
+++ spec/tic_tac_toe_spec.rb
@@ -1,6 +1,27 @@
 require_relative '../lib/tic_tac_toe'
 
 describe Game do
+  describe '#run' do
+    subject(:game) { described_class.new }
+
+    before do
+      allow(game).to receive(:play)
+    end
+
+    it 'plays game once when play_again is false' do
+      allow(game).to receive(:play_again?).and_return(false)
+      expect(game).to receive(:play).once
+      game.run
+    end
+
+    it 'plays game twice when play_again is true, then false' do
+      allow(game).to receive(:play_again?).and_return(true, false)
+      expect(game).to receive(:play).twice
+      game.run
+    end
+  end
+
+  describe '#play'
   describe '#play_round'
   describe '#switch_player'
   describe '#validate_input'
